d4c0cbd1616aa16619708af2c91c49c59e5d7379
arfaghifari <naufal.ghifari@tokopedia.com>
2023-11-29 16:34:08 +0700

    feat(refactor): pretty response builder

--- src/handlers/http/adventurer/adventurer.go
+++ src/handlers/http/adventurer/adventurer.go
@@ -2,12 +2,26 @@ package adventurer
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 
 	model "github.com/arfaghifari/guild-board/src/model/adventurer"
 	usecase "github.com/arfaghifari/guild-board/src/usecase/adventurer"
 )
+
+type Header struct {
+	Error      string `json:"error_code"`
+	StatusCode int    `json:"status_code"`
+}
+
+type MessageResponse struct {
+	Header `json:"header"`
+	Data   SuccesMessage `json:"data"`
+}
+
+type SuccesMessage struct {
+	Success bool `json:"success"`
+}
 
 type Handlers interface {
 	CreateAdventurer(http.ResponseWriter, *http.Request)
@@ -25,29 +39,80 @@ func NewHandlers() (Handlers, error) {
 }
 
 func (h *handlers) CreateAdventurer(w http.ResponseWriter, r *http.Request) {
-	var adventurer model.Adventurer
+	var (
+		statusCode = http.StatusBadRequest
+		resp       MessageResponse
+		adventurer model.Adventurer
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 
 	if err := json.NewDecoder(r.Body).Decode(&adventurer); err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
 		return
 	}
+	if adventurer.Name == "" || adventurer.Rank <= 0 {
+		resp.Header.Error = "name and rank are required and must be valid"
+		return
+	}
+
 	err := h.usecase.CreateAdventurer(adventurer)
 
-	if err == nil {
-		fmt.Fprintf(w, "success")
+	if err != nil {
+		statusCode = 500
+		resp.Header.Error = err.Error()
+		return
 	}
+	statusCode = 200
+	resp.Data.Success = true
 }
 
 func (h *handlers) UpdateAdventurerRank(w http.ResponseWriter, r *http.Request) {
-	var adventurer model.Adventurer
+	var (
+		statusCode = http.StatusBadRequest
+		resp       MessageResponse
+		adventurer model.Adventurer
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 
 	if err := json.NewDecoder(r.Body).Decode(&adventurer); err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
 		return
 	}
+
+	if adventurer.ID <= 0 || adventurer.Rank <= 0 {
+		resp.Header.Error = "id and rank are required and must be valid"
+		return
+	}
+
 	err := h.usecase.UpdateAdventurerRank(adventurer)
 
-	if err == nil {
-		fmt.Fprintf(w, "success")
+	if err != nil {
+		statusCode = 500
+		resp.Header.Error = err.Error()
+		return
 	}
+	statusCode = 200
+	resp.Data.Success = true
 }
--- src/handlers/http/quest/quest.go
+++ src/handlers/http/quest/quest.go
@@ -21,6 +21,15 @@ type GetQuestByStatusResponse struct {
 	Data   []model.GetQuestByStatus `json:"data"`
 }
 
+type MessageResponse struct {
+	Header `json:"header"`
+	Data   SuccesMessage `json:"data"`
+}
+
+type SuccesMessage struct {
+	Success bool `json:"success"`
+}
+
 type Handlers interface {
 	GetQuestByStatus(http.ResponseWriter, *http.Request)
 	CreateQuest(http.ResponseWriter, *http.Request)
@@ -47,127 +56,278 @@ func GetHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *handlers) GetQuestByStatus(w http.ResponseWriter, r *http.Request) {
+	var (
+		statusCode = http.StatusBadRequest
+		resp       GetQuestByStatusResponse
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 	status, err := strconv.Atoi(r.URL.Query().Get("status"))
 	if err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
+		return
+	}
+	if status != 0 && status != 1 {
+		resp.Header.Error = "Invalid status number"
 		return
 	}
 	res, err := h.usecase.GetQuestByStatus(int32(status))
 	if err != nil {
-		http.Error(w, "internal server error", 500)
+		statusCode = 500
+		resp.Header.Error = err.Error()
 		return
 	}
-	resp, _ := json.Marshal(GetQuestByStatusResponse{
-		Header: Header{
-			Error:      "",
-			StatusCode: 200,
-		},
-		Data: res,
-	})
-
-	w.Write(resp)
+	statusCode = 200
+	resp.Data = res
 
 }
 
 func (h *handlers) CreateQuest(w http.ResponseWriter, r *http.Request) {
-	var quest model.Quest
+	var (
+		statusCode = http.StatusBadRequest
+		resp       MessageResponse
+		quest      model.Quest
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 
 	if err := json.NewDecoder(r.Body).Decode(&quest); err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
 		return
 	}
+	if quest.Name == "" || quest.MinimumRank <= 0 || quest.RewardNumber <= 0 {
+		resp.Header.Error = "name, minimum_rank and reward_number are required and must be valid"
+		return
+	}
+
 	err := h.usecase.CreateQuest(quest)
 
 	if err != nil {
-		http.Error(w, "internal server error", 500)
+		statusCode = 500
+		resp.Header.Error = err.Error()
 		return
 	}
-
-	fmt.Fprintf(w, "success")
+	statusCode = 200
+	resp.Data.Success = true
 
 }
 
 func (h *handlers) DeleteQuest(w http.ResponseWriter, r *http.Request) {
-	var quest model.Quest
+	var (
+		statusCode = http.StatusBadRequest
+		resp       MessageResponse
+		quest      model.Quest
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 
 	if err := json.NewDecoder(r.Body).Decode(&quest); err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
 		return
 	}
+
+	if quest.ID <= 0 {
+		resp.Header.Error = "quest_id is required and must be valid"
+		return
+	}
+
 	err := h.usecase.DeleteQuest(quest)
 
 	if err != nil {
-		http.Error(w, "internal server error", 500)
+		statusCode = 500
+		resp.Header.Error = err.Error()
 		return
 	}
-
-	fmt.Fprintf(w, "success")
+	statusCode = 200
+	resp.Data.Success = true
 }
 
 func (h *handlers) UpdateQuestRank(w http.ResponseWriter, r *http.Request) {
-	var quest model.Quest
+	var (
+		statusCode = http.StatusBadRequest
+		resp       MessageResponse
+		quest      model.Quest
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 
 	if err := json.NewDecoder(r.Body).Decode(&quest); err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
 		return
 	}
+
+	if quest.ID <= 0 || quest.MinimumRank <= 0 {
+		resp.Header.Error = "quest_id and minimum_rank are required and must be valid"
+		return
+	}
+
 	err := h.usecase.UpdateQuestRank(quest)
 
 	if err != nil {
-		http.Error(w, "internal server error", 500)
+		statusCode = 500
+		resp.Header.Error = err.Error()
 		return
 	}
-
-	fmt.Fprintf(w, "success")
+	statusCode = 200
+	resp.Data.Success = true
 }
 
 func (h *handlers) UpdateQuestReward(w http.ResponseWriter, r *http.Request) {
-	var quest model.Quest
+	var (
+		statusCode = http.StatusBadRequest
+		resp       MessageResponse
+		quest      model.Quest
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 
 	if err := json.NewDecoder(r.Body).Decode(&quest); err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
 		return
 	}
+
+	if quest.ID <= 0 || quest.RewardNumber <= 0 {
+		resp.Header.Error = "quest_id and reward_number are required and must be valid"
+		return
+	}
+
 	err := h.usecase.UpdateQuestReward(quest)
 
 	if err != nil {
-		http.Error(w, "internal server error", 500)
+		statusCode = 500
+		resp.Header.Error = err.Error()
 		return
 	}
-
-	fmt.Fprintf(w, "success")
+	statusCode = 200
+	resp.Data.Success = true
 }
 
 func (h *handlers) TakeQuest(w http.ResponseWriter, r *http.Request) {
-	var takeByRequest model.TakenBy
+	var (
+		statusCode    = http.StatusBadRequest
+		resp          MessageResponse
+		takeByRequest model.TakenBy
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 
 	if err := json.NewDecoder(r.Body).Decode(&takeByRequest); err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
 		return
 	}
+
+	if takeByRequest.AdventurerID <= 0 || takeByRequest.QuestID <= 0 {
+		resp.Header.Error = "adv_id and quest_id are required and must be valid"
+		return
+	}
+
 	err := h.usecase.TakeQuest(takeByRequest.QuestID, takeByRequest.AdventurerID)
 
 	if err != nil {
-		http.Error(w, "internal server error", 500)
+		statusCode = 500
+		resp.Header.Error = err.Error()
 		return
 	}
-
-	fmt.Fprintf(w, "success")
+	statusCode = 200
+	resp.Data.Success = true
 }
 
 func (h *handlers) ReportQuest(w http.ResponseWriter, r *http.Request) {
-	var reportQuest model.ReportQuest
+	var (
+		statusCode  = http.StatusBadRequest
+		resp        MessageResponse
+		reportQuest model.ReportQuest
+	)
+	defer func() {
+		resp.StatusCode = statusCode
+		responseWriter, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatal("Failed build response")
+		}
+		if statusCode == http.StatusOK {
+			w.Write(responseWriter)
+		} else {
+			http.Error(w, string(responseWriter), statusCode)
+		}
+	}()
 
 	if err := json.NewDecoder(r.Body).Decode(&reportQuest); err != nil {
-		http.Error(w, "bad request", 400)
+		resp.Header.Error = err.Error()
 		return
 	}
+
+	if reportQuest.AdventurerID <= 0 || reportQuest.QuestID <= 0 {
+		resp.Header.Error = "adv_id and quest_id are required and must be valid"
+		return
+	}
+
 	err := h.usecase.ReportQuest(reportQuest.QuestID, reportQuest.AdventurerID, reportQuest.IsCompleted)
 
 	if err != nil {
-		http.Error(w, "internal server error", 500)
+		statusCode = 500
+		resp.Header.Error = err.Error()
 		return
 	}
-
-	fmt.Fprintf(w, "success")
+	statusCode = 200
+	resp.Data.Success = true
 }
